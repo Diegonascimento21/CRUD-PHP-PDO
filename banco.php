94fc2b3fc7b6624f1bbfc85f56e97cc91065197a
diego nascimento <diegonasc21@gmail.com>
2020-01-09 22:08:47 -0200

    Metodos Update & Delete

--- banco.php
+++ banco.php
@@ -4,6 +4,7 @@
         private $pdo;
         private $numRows;
         private $array;
+        private $sql;
 
         public function __construct($host, $dbname, $dbuser, $dbpassword){
             try{
@@ -32,14 +33,51 @@
     //Método insert 
         public function insert($table, $data){
             if(!empty($table) && (is_array($data) && count($data) > 0)){
-                $sql = "INSERT INTO".$table."SET";
+                $sql = " INSERT INTO ".$table." SET ";
                 $dados = Array();
                 foreach ($data as $key => $value) {
                     $dados[] = $key."= '". addslashes($value)."'";
                 }
-                $sql = sql.implode(",", $dados);
+                $sql =$sql.implode(",", $dados);
                 $this->pdo->query($sql);
             }
         }
+
+    //Método Update
+        public function update($table, $data, $where = array(), $where_cond = 'AND'){
+            if(!empty($table) && (is_array($data) && count($data) > 0) && is_array($where)){
+                $sql = "UPDATE ".$table." SET ";
+                $dados = Array();
+                foreach ($data as $key => $value) {
+                    $dados[] = $key."= '". addslashes($value)."'";
+                }
+                    $sql = $sql.implode(",", $dados);
+
+                if(count($where) > 0){
+                    $dados = Array();
+                    foreach ($where as $key => $value) {
+                        $dados[] = $key."= '". addslashes($value)."'";
+                    }
+                    $sql =$sql." WHERE ".implode(" ".$where_cond." ", $dados);
+                }    
+                $this->pdo->Query($sql);               
+            }
+        }
+    //Delete
+        public function delete($table, $where, $where_cond = "AND"){
+            if(!empty($table) && (is_array($where) && count($where) > 0)){
+                $sql = "DELETE FROM ".$table;
+                if(count($where) > 0){
+                    $dados = Array();
+                    foreach ($where as $key => $value) {
+                        $dados[] = $key."= '". addslashes($value)."'";
+                    }
+                    $sql =$sql." WHERE ".implode(" ".$where_cond." ", $dados);               
+                } 
+                $this->pdo->query($sql);
+            }         
+        }
+
+
     }
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,25 @@
 <?php
     require "banco.php";
     $banco = new Banco("localhost", "TestesCRUD", "root", "");
+    /*
+    $banco->insert("usuarios", array(  //insert
+        "nome" => 'tereza',
+        "sobrenome" => 'vieira',
+        "email" => 'tereza.vieira08@gmail.com'
+    ));
+
+
+    $banco->update("usuarios", //tabela, nova informaçao, onde vai ser atualizado
+        array("nome" => "JOAO"),
+        array("id " => "1")
+           
+    );
     
+    $banco->query("SELECT * FROM usuarios"); //query select
+    print_r($banco->result()); //traz o resultado da query
     
+
+    $banco->delete("usuarios", array("id"=>"1")); //delete
+
+    */
 ?>
